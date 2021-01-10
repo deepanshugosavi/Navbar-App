42ffdd6ef0e20bc8faf40aa40697f8938c2cffca
Deepanshu Gosavi <gosavideepanshu@gmail.com>
2021-01-10 13:57:16 +0530

    Favicon of App Added

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,15 @@ import Graphs from "./screens/Graphs";
 import User from "./screens/User";
 
 function App(props) {
+  const base_app_path = "/Navbar-App";
   return (
     <div className="global__container">
       <Navbar />
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/products" component={Products} />
-        <Route exact path="/graphs" component={Graphs} />
-        <Route exact path="/user" component={User} />
+        <Route exact path={base_app_path + "/"} component={Home} />
+        <Route exact path={base_app_path + "/products"} component={Products} />
+        <Route exact path={base_app_path + "/graphs"} component={Graphs} />
+        <Route exact path={base_app_path + "/user"} component={User} />
       </Switch>
     </div>
   );
